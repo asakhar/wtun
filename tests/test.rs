1c47508fc946b96d2f567bac5ae2344cdac3538a
asakhar <a@sakhar.ru>
2023-07-27 18:19:25 +0300

    remove redundant test

--- tests/test.rs
+++
@@ -1,9 +0,0 @@
-use simple_logger::SimpleLogger;
-use wtun::*;
-
-#[test]
-fn creates_adapter() {
-  drop(SimpleLogger::new().init());
-  let test_guid = None;
-  let _adapter = Adapter::create("test", "test type", test_guid).unwrap();
-}
